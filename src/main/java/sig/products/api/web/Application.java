44087d80ca46175066b7261726b8f8a16285f528
danielysyeung <danielysyeung@gmail.com>
2016-10-04 13:13:48 -0700

    initial checkin

---
+++ src/main/java/sig/products/api/web/Application.java
@@ -0,0 +1,12 @@
+package sig.products.api.web;
+
+import javax.ws.rs.ApplicationPath;
+import org.glassfish.jersey.server.ResourceConfig;
+
+@ApplicationPath("api")
+public class Application extends ResourceConfig {
+
+	public Application() { 
+		
+	}
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+<h2>SIG Products API</h2>
+</body>
+</html>
